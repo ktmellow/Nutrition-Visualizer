edf6413ddd8cdeda9073c69eba3d363503f7ccb2
ktmellow <letsspamkt@gmail.com>
2016-07-18 15:50:58 -0700

    updated client routing

--- client/javascripts/app.js
+++ client/javascripts/app.js
@@ -4,16 +4,16 @@ app.config(config)
 
 function config($routeProvider, $locationProvider) {
   $routeProvider
-    .when('/eval', {
+    .when('/', {
       templateUrl: 'client/views/eval.html',
       controller: 'EvalController',
       controllerAs: 'vm'
     })
-    .when('/', {
-      templateUrl: 'client/views/index.html',
-      controller: 'NutritionController',
-      controllerAs: 'vm'
-    })    
+    // .when('/', {
+    //   templateUrl: 'client/views/index.html',
+    //   controller: 'NutritionController',
+    //   controllerAs: 'vm'
+    // })    
     .otherwise({redirectTo: '/'})
   $locationProvider.html5Mode(true);
 }
